87e42c641e5c8b6dc87a15f273121f6e6ff42473
Nick Boyadjian <nboyadjian95@gmail.com>
2021-10-13 20:48:41 -0400

    init

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,9 @@
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function About() {
   return (
     <div className={styles.container}>
-      Home page
+      About page
     </div>
   )
 }
